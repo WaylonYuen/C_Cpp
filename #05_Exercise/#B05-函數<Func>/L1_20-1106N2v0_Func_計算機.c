409c116680160fd59f48aab51e8622250e12610b
Waylon <1664940243LWY@gmail.com>
2020-12-09 00:39:32 +0800

    Add : L1_20-1106N2v0_Func_計算機.c

--- #05_Exercise/#B05-函數<Func>/L1_20-1106N2v0_Func_計算機.c
+++ #05_Exercise/#B05-函數<Func>/L1_20-1106N2v0_Func_計算機.c
@@ -1,4 +1,29 @@
-//利用副程式寫一個計算機
+// 編號：L1_20-1106N2v0_Func_計算機.c
+
+// 程式類別：C語言
+// 基礎題：陣列練習 ( 難度 ★☆☆☆☆ ) -> (tmp: ★ ☆)
+// 題目：計算機
+
+// 時間：NULL ( 不限 ) //最佳 3s內
+// 內存大小：128MB
+
+// 題目內容：
+// 輸入兩筆整數及運算符編號, 再利用副程式計算整數加減乘除(每個副程式負責一個運算)並回傳結果.
+
+// 輸入：
+// 兩筆整數 及 運算符編號
+
+// 輸出：
+// 運算結果
+
+// Sample input :
+// 13
+// 2
+// 3
+
+// Sample output： 
+// 26
+
 #include <stdio.h>
 
 //副程式宣告
@@ -13,7 +38,6 @@ int Div(int a, int b);
 int main(void) {
 
     int result = Calculator();
-
     printf("計算結果: %d\n", result);
 
     return 0;
@@ -37,11 +61,8 @@ int Calculator(void) {
     switch (request) {
 
         case 1: ans = Add(a, b); break;
-
         case 2: ans = Sub(a, b); break;
-
         case 3: ans = Mul(a, b); break;
-
         case 4: ans = Div(a, b); break;
         
         default:
@@ -62,6 +83,7 @@ void Menu(void) {
     printf(" - 4 - 除法\n");
     printf("================End\n");
 }
+
 //加法副程式
 int Add(int a, int b) {
     return a + b;
